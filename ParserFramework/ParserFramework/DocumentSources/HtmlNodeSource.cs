a007ceafc0e59d808f948133a3d548e518e68906
AndreyOOP <andrew_yudaichev@i.ua>
2021-03-28 23:15:10 +0300

    SL-1 | IHtmlNodeSource & Implementation

---
+++ ParserFramework/ParserFramework/DocumentSources/HtmlNodeSource.cs
@@ -0,0 +1,29 @@
+﻿using HtmlAgilityPack;
+using ParserFramework.Abstractions;
+
+namespace ParserFramework
+{
+    public class HtmlNodeSource : IHtmlNodeSource
+    {
+        private string html;
+        private HtmlNode htmlNode;
+
+        public HtmlNodeSource(string html)
+        {
+            this.html = html;
+        }
+
+        public HtmlNode HtmlDocument 
+        {
+            get
+            {
+                if (htmlNode == null){
+                    var htmlDocument = new HtmlDocument();
+                    htmlDocument.LoadHtml(html);
+                    htmlNode = htmlDocument.DocumentNode;
+                }
+                return htmlNode;
+            }
+        }
+    }
+}
---
+++ ParserFramework/ParserFramework/DocumentSources/UriNodeSource.cs
@@ -0,0 +1,38 @@
+﻿using HtmlAgilityPack;
+using ParserFramework.Abstractions;
+using System;
+using System.Net.Http;
+
+namespace ParserFramework
+{
+    public class UriNodeSource : IHtmlNodeSource
+    {
+        private Uri uri;
+        private HttpClient httpClient;
+        private HtmlNode htmlNode;
+
+        public UriNodeSource(Uri uri, HttpClient httpClient)
+        {
+            this.uri = uri;
+            this.httpClient = httpClient;
+        }
+
+        public HtmlNode HtmlDocument
+        {
+            get
+            {
+                if (htmlNode != null)
+                    return htmlNode;
+
+                var response = httpClient.GetAsync(uri).Result;
+                if (response.IsSuccessStatusCode)
+                {
+                    var html = response.Content.ReadAsStringAsync().Result;
+                    htmlNode = new HtmlNodeSource(html).HtmlDocument;
+                    return htmlNode;
+                }
+                throw new HttpRequestException($"Request to {uri} return {response.StatusCode} status code");
+            }
+        }
+    }
+}
