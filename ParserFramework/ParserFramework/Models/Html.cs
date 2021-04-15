fe37f90056977ddd49dff1974968b6368a37ecc2
AndreyOOP <andrew_yudaichev@i.ua>
2021-04-16 01:34:51 +0300

    SL-1 | Add Models

---
+++ ParserFramework/ParserFramework/Models/Html.cs
@@ -0,0 +1,19 @@
+﻿namespace ParserFramework.Models
+{
+    /// <summary>
+    /// Wrapper to avoid flg usage isHtml in XPathAttribute 
+    /// </summary>
+    public class Html
+    {
+        string html;
+
+        public Html(string html)
+        {
+            this.html = html;
+        }
+
+        public override string ToString() => html;
+
+        public static implicit operator string(Html html) => html.ToString();
+    }
+}
---
+++ ParserFramework/ParserFramework/Models/PropertyType.cs
@@ -0,0 +1,46 @@
+﻿namespace ParserFramework.Models
+{
+    /// <summary>
+    /// Supported types for autoconvertion
+    /// </summary>
+    public enum PropertyType
+    {
+        Int,
+
+        Bool,
+
+        Decimal,
+
+        /// <summary>
+        /// Type which contain string representation of the <see cref="HtmlNode"/>
+        /// </summary>
+        Html,
+
+        String,
+
+        /// <summary>
+        /// Model which should be reccursively parsed till other primitive types
+        /// </summary>
+        Model,
+
+        /// <summary>
+        /// <see cref="HtmlNode"/> itself
+        /// </summary>
+        HtmlNode,
+
+        IntCollection,
+
+        BoolCollection,
+
+        DecimalCollection,
+
+        HtmlCollection,
+
+        StringCollection,
+
+        ModelCollection,
+
+        HtmlNodeCollection
+    }
+
+}
